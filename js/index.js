945ce7369f876902dc51679b3067469eb16ec7fe
herunsheng2 <herunsheng2@yy.com>
2022-02-09 17:10:17 +0800

    feat: update js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    console.log('load ready')
+})
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('DOMContentLoaded ready')
+})
